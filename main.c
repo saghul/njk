9470dc61edcda6522261fad8c79221fb36fdaa58
Saúl Ibarra Corretgé <s@saghul.net>
2021-06-01 16:29:53 +0200

    core: wire up QuickJS to njk

--- main.c
+++ main.c
@@ -1,3 +1,21 @@
-int main() {
-    return 0;
+
+#include "quickjs/quickjs-libc.h"
+
+
+int main(int argc, char **argv) {
+    JSRuntime *rt;
+    JSContext *ctx;
+
+    rt = JS_NewRuntime();
+    js_std_init_handlers(rt);
+
+    ctx = JS_NewContext(rt);
+    js_std_add_helpers(ctx, argc, argv);
+
+    js_std_loop(ctx);
+
+    JS_FreeContext(ctx);
+    JS_FreeRuntime(rt);
+
+  return 0;
 }
