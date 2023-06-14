d119c9803ad7d736aa28bcb872b78b21bdbfd704
Levi Pesin <35454228+LeviPesin@users.noreply.github.com>
2023-06-15 06:09:50 +1000

    Add `toJSON` filter

--- main.js
+++ main.js
@@ -17,6 +17,10 @@ function exitIfError(errno) {
     }
 }
 
+function toJSON(str) {
+    return JSON.parse(str);
+}
+
 
 // Template loader for Nunjucks.
 //
@@ -81,6 +85,7 @@ const env = std.getenviron();
 delete env['_'];
 const isDev = Boolean(env.NJK_DEV);
 const nenv = new nunjucks.Environment(new FSLoader(templateDir, { noCache: true }), { autoescape: false, dev: isDev });
+nenv.addFilter('toJSON', toJSON);
 const res = nenv.render(templateName, { env });
 
 std.out.puts(res);
